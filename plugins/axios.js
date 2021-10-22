d90fc0d4b725e2afdd9134295f5355185bf62015
EasonChang <s8710606@gmail.com>
2021-10-23 03:13:29 +0800

    fix: fixed some error

--- plugins/axios.js
+++ plugins/axios.js
@@ -12,17 +12,6 @@ export default function ({ app, $axios, redirect, store, cookies, error }) {
       return res.data;
     }
   });
-  $axios.onError((error) => {
-    switch (error.response.status) {
-      case 403:
-      case 404:
-        error({ statusCode: 404, message: '找不到頁面' });
-        break;
-      case 500:
-        error({ statusCode: 500, message: '服務器出現錯誤，請稍後再試' });
-        break;
-      default:
-        break;
-    }
-  });
+  // $axios.onError((e) => {
+  // });
 }
